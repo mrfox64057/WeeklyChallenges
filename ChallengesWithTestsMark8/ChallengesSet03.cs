f7413e4728d6e8dc1541ee33605d42b6e60f1b18
Adam Jankowski <ghost17814@gmail.com>
2023-05-05 10:54:50 -0400

    ughhhhhhhhhhhhhhhhhhhhhhhh

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -8,17 +8,17 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            if (vals == null) 
-            { 
-                return false;
-            }
+            //if (vals == null) 
+            //{ 
+            //    return false;
+            //}
 
             foreach (var item in vals)
             {
-                //if(var item == false in vals)
-                //{
-                //    return true;
-                //}
+                if (item == false)
+                {
+                    return true;
+                }
             }
             return false;
             //return vals != null && vals.Length != 0 && vals.Contains(false);
@@ -26,22 +26,22 @@ namespace ChallengesWithTestsMark8
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            if (numbers == null)
-            {
-                return false;
-            }
+            //if (numbers == null)
+            //{
+            //    return false;
+            //}
 
             int sum = 0;
 
-            foreach (var number in numbers)
-            {
-                if (number % 2 != 0) 
-                {
-                    sum += number;
-                }
-            } 
-            return sum % 2 != 0;//same as if else below
-            //if (sum % 2 != 0) 
+            //foreach (var number in numbers)
+            //{
+            //    if (number % 2 != 0) 
+            //    {
+            //        sum += number;
+            //    }
+            //} 
+            //return sum % 2 != 0;//same as if else below
+            //if (sum % 2 != 0)
             //{
             //    return true;
             //}
@@ -49,49 +49,144 @@ namespace ChallengesWithTestsMark8
             //{
             //    return false;
             //}
-            //return numbers != null && numbers.Where(x => x % 2 != 0).Sum() % 2 != 0;
-           //if(numbers == null || numbers.Count() == 0)
-           // {
-           //     return false;
-           // }
+            return numbers != null && numbers.Where(x => x % 2 != 0).Sum() % 2 != 0;
+            //if (numbers == null || numbers.Count() == 0)
+            //{
+            //    return false;
+            //}
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
+            //return password.Any(c => char.IsLetter(c)) && password.Any(c => char.IsDigit(c)) && password.Any(c => char.IsUpper(c));
+
             var isLower = false;
-            var isLowerUpper = false;
-            var isNumber = false;
-            return false;
+            var isDigit = false;
+            var isUpper = false;
+            foreach (var letter in password)
+            {
+                if (char.IsLower(letter))
+                {
+                    isLower = true;
+                }
+                if (char.IsDigit(letter))
+                {
+                    isDigit = true;
+                }
+                if (char.IsUpper(letter))
+                {
+                    isUpper = true;
+                }
+            }
+            if (isLower && isDigit && isUpper)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(val))
+            {
+                throw new ArgumentException();
+            }
+            return val[0];
+
+            //oneLine  //public char GetFirstLetterOfString(string val) => string.IsNullOrEmpty(val) ? throw new ArgumentException() : val.First(c => char.IsLetterOrDigit(c));
+
+            //if (string.IsNullOrEmpty(val))
+            //{
+            //    throw new ArgumentException();
+            //}
+
+            //char firstChar = val[0];
+
+            //if (!char.IsLetterOrDigit(firstChar))
+            //{
+            //    throw new ArgumentException();
+            //}
+
+            //return firstChar;
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+
+            return val[val.Length - 1];
+            //oneLine  public char GetLastLetterOfString(string val) => string.IsNullOrEmpty(val) ? throw new ArgumentException() : val.Last(c => char.IsLetterOrDigit(c));
+
+            //if (string.IsNullOrEmpty(val))
+            //{
+            //    throw new ArgumentException();
+            //}
+
+            //char lastChar = val[val.Length - 1];
+
+            //if (!char.IsLetterOrDigit(lastChar))
+            //{
+            //    throw new ArgumentException();
+            //}
+
+            //return lastChar;
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            return divisor == 0 ? 0 : dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            return Enumerable.Range(1, 100).Where(x => x % 2 != 0).ToArray();
+            //int[] result = new int[50];
+            //int count = 0;
+            //int num = 1;
+            //do
+            //{
+            //    if (num % 2 != 0)
+            //    {
+            //        result[count] = num;
+            //        count++;
+            //    }
+            //    num++;
+            //} while (count < 50);
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    words[i] = words[i].ToUpper();
+            //}
+
+            //oneLine  words = words.Select(w => w.ToUpper()).ToArray();  
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                switch (words[i])
+                {
+                    case null:
+                        words[i] = null;
+                        break;
+                    case "":
+                        words[i] = "";
+                        break;
+                    default:
+                        words[i] = words[i].ToUpper();
+                        break;
+                }
+            }
+
+
         }
     }
 }
